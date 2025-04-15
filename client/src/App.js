2733c76522ac9f6b69f75dde7f8dec924f850ffb
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2025-04-15 13:26:09 -0600

    Footer basico

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Aboutme from './pages/Aboutme';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/aboutme' element={<Aboutme/>}/>
       </Routes>
+      <Footer/>
     </div>
   );
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './styles_components/Footer.css'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='container-footer'>
+      <ul className='list'>
+        <li className='item'>
+          Trello creado por Francisco Rocha Sierra
+        </li>
+        <li className='item'>
+          Gracias por visitarlo
+        </li>
+        <br />
+        <li className='item'>
+          Para ver mas de mis trabajos visita
+        </li>
+        <li className='item'>
+          <a href="https://github.com/P4NCH1T0?tab=overview&from=2025-04-01&to=2025-04-15">Prtafolio</a>    
+        </li>
+      </ul>
+    </footer>
   )
 }
 
